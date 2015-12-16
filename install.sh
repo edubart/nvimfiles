ff1825ccee7fd241b88f8944319df518d1c9a9f5
Eduardo Bart <edub4rt@gmail.com>
2015-12-16 17:13:44 -0200

    Change install script

--- install.sh
+++ install.sh
@@ -18,37 +18,6 @@ die() {
   exit 1
 }
 
-# Minifies vim script files in place
-minify_vim_script_file_in_place() {
-  # 1. Convert tabs to spaces
-  # 2. Remove blank lines
-  # 3. Remove beginning spaces
-  # 4. Remove whole line comments
-  # 5. Remove trailing spaces
-
-  # Set LANG as C to treat all ASCII characters as themselves and all
-  # non-ASCII characters as literals
-  env LANG=C sed 's/	/ /g' "$1" |
-    env LANG=C sed '/^$/d' |
-    env LANG=C sed 's/^[ ]*//' |
-    env LANG=C sed '/^[ ]*"/d' |
-    env LANG=C sed 's/[ ]*$//' > tmp
-
-  # Copy tmp to the given file and remove tmp
-  cp tmp "$1"
-  rm tmp
-}
-
-# Returns the source file given a symlink target file
-readlink_posix() {
-  target_file="$1"
-  if [ -h "$target_file" ]; then
-    ls_res="$(LC_TIME=C ls -ld "$target_file")"
-    source_file=${ls_res#*"${target_file} -> "}
-    echo "$source_file"
-  fi
-}
-
 here=$(dirname "$0") && here=$(cd "$here" && pwd -P)
 
 while :
@@ -88,90 +57,19 @@ do
   esac
 done
 
-# Back up any existing configurations
-echo
-echo "Backing up any existing configurations"
+DEST="${HOME}/.config/nvim"
+rm -rf $DEST
+mkdir -p $DEST/bundle
+ln -s "${here}/vimrc" "$DEST/init.vim"
 
-# Actually copy the contents of the symlinks (or regular files), not just the
-# link if exists
-for file in "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
-  if [ -e "$file" ]; then
-    cp -v "$file" "${file}.old" || die "Could not copy ${file} to ${file}.old"
-  fi
-done
-
-if [ -h "${HOME}/.vim" ] && 
-   [ -e $(readlink_posix "${HOME}/.vim") ]; then
-  # Remove any existing ~/.vim.old because of `cp` symlink issues
-  rm -rf "${HOME}/.vim.old"
-  echo "Copying contents of symlink ${HOME}/.vim to ${HOME}/.vim.old"
-  cp -R "$(readlink_posix "${HOME}/.vim")" "${HOME}/.vim.old" \
-    || die "Could not copy ${HOME}/.vim to ${HOME}/.vim.old"
-else
-  if [ -d "${HOME}/.vim" ]; then
-    echo "Renaming ${HOME}/.vim to ${HOME}/.vim.old"
-    mv "${HOME}/.vim" "${HOME}/.vim.old" \
-      || die "Could not move ${HOME}/.vim to ${HOME}/.vim.old"
-  fi
-fi
-
-echo
-echo "Minifying *.vim files"
-
-# Remove any existing build/vim.min because of `cp` symlink issues
-rm -rf "${here}/build/vim.min"
-mkdir -p "${here}/build/vim.min/bundle"
-
-# Consolidate all plugins into bundle and save pathogen from having to load
-# multiple paths
 for file in "$here"/*; do
   dir_name="$(basename "$file")"
 
-  if [ -d "$file" ] &&
-     [ "$dir_name" != "build" ] &&
-     [ "$dir_name" != "bin" ] &&
-     [ "$dir_name" != "vimrc-labs" ]; then
+  if [ -d "$file" ]; then
     for plugin in "$file"/*; do
       plugin_name="$(basename "$plugin")"
-      cp -R "$plugin" "${here}/build/vim.min/bundle/${plugin_name}"
+      ln -s "$plugin" "$DEST/bundle/$plugin_name"
     done
   fi
 done
 
-# Copy vim files, locally, to be minified
-
-cp "${here}/gvimrc" "${here}/build/gvimrc.min"
-
-# Concatenate vimrc and any vimrc-labs/vimrc into build/vimrc.min
-touch "${here}/build/vimrc.min"
-cat /dev/null > "${here}/build/vimrc.min"
-cat "${here}/vimrc" >> "${here}/build/vimrc.min"
-
-if [ -d "${here}/vimrc-labs/vimrc" ]; then
-  cat "${here}/vimrc-labs/vimrc" >> "${here}/build/vimrc.min"
-fi
-
-# Minify
-minify_vim_script_file_in_place "${here}/build/vimrc.min"
-minify_vim_script_file_in_place "${here}/build/gvimrc.min"
-#find "${here}/build/vim.min" -name "*.vim" |
-#  while read file; do minify_vim_script_file_in_place "$file"; done
-
-# Link to minified configurations
-ln -sfv "${here}/build/vimrc.min" "${HOME}/.vimrc"
-ln -sfv "${here}/build/gvimrc.min" "${HOME}/.gvimrc"
-ln -sfvn "${here}/build/vim.min" "${HOME}/.vim"
-
-echo
-echo "Linking files in bin/"
-
-# Create $HOME/bin if it doesn't already exist
-if [ ! -d "${HOME}/bin" ]; then
-  mkdir -v "${HOME}/bin"
-fi
-
-# Link files in bin to $HOME/bin
-for file in "${here}/bin"/*; do
-  filename="$(basename "$file")"
-  ln -sfv "${here}/bin/${filename}" "${HOME}/bin/${filename}"
-done
